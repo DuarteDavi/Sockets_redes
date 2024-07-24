45d82cd099fa5be3454613eca5d6a50a47099373
pablovas <pablo71042001@gmail.com>
2024-07-24 13:45:47 -0300

    Feat:added messages structure

--- Client.py
+++ Client.py
@@ -1,6 +1,7 @@
 import socket
+import time
 
-def start_client(server_host='10.10.56.8', server_port=42647):
+def start_client(server_host='10.10.56.8', server_port=28909):
     # Criar um socket TCP
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -9,39 +10,34 @@ def start_client(server_host='10.10.56.8', server_port=42647):
     print(f"Conectado ao servidor em {server_host}:{server_port}")
     client_socket.settimeout(10.0)
     
-    while True:
-       
-        message = input("Digite a sua mensagem (Digite 'sair' para sair): ")  # Enviar uma mensagem
-        if message.lower() == 'sair' :
-            break
+    try:
+        while True:
+            recipient_id = input("Digite o ID do destinatário (13 dígitos): ")
+            if len(recipient_id) != 13:
+                print("O ID do destinatário deve ter exatamente 13 dígitos.")
+                continue
 
-        elif  (message.startswith('03')) :
-             client_socket.sendall(message.encode())
-            
-        else:
-            continue
-        
+            message_content = input("Digite o conteúdo da mensagem (máximo de 218 caracteres): ")
+            if len(message_content) > 218:
+                print("O conteúdo da mensagem deve ter no máximo 218 caracteres.")
+                continue
 
+            # Verifica se o usuário deseja sair antes de formatar e enviar a mensagem
+            if recipient_id.lower() == 'sair' or message_content.lower() == 'sair':
+                break
 
-        
+            timestamp = time.time()
+            # Formatar a mensagem com '03', IP do cliente, ID do destinatário, timestamp e conteúdo da mensagem
+            formatted_message = f'03{client_socket.getsockname()[0].replace(".", "")}{recipient_id}{int(timestamp)}{message_content.replace(" ", "_")}'
+            client_socket.sendall(formatted_message.encode())
+            print("Mensagem enviada ao servidor:", formatted_message)
 
-            
-        
-            
-        
-            
-
-       
-
-        # Receber a resposta do servidor
-        data = client_socket.recv(1024)
-        print("Recebido do servidor:", data.decode())
-
-    # Fecha o socket
-    client_socket.close()
-
-
+            # Receber a resposta do servidor
+            data = client_socket.recv(1024)
+            print("Recebido do servidor:", data.decode())
+    finally:
+        # Fecha o socket independentemente de como o loop while termina
+        client_socket.close()
 
 if __name__ == '__main__':
-    start_client(server_host='10.10.56.8', server_port=42647)
-
+    start_client(server_host='10.10.56.8', server_port=28909)
