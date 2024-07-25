31532884ba26c6959f0997840360cb624c8b3534
pablovas <pablo71042001@gmail.com>
2024-07-25 13:23:11 -0300

    Hotfix

--- Client.py
+++ Client.py
@@ -1,39 +1,20 @@
 import socket
 
-def start_client(server_host='10.10.56.8', server_port=42647):
+def start_client(server_host='10.228.247.158', server_port=9941):
     # Criar um socket TCP
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Conectar ao servidor
     client_socket.connect((server_host, server_port))
     print(f"Conectado ao servidor em {server_host}:{server_port}")
-    client_socket.settimeout(10.0)
+    client_socket.settimeout(10.0) 
     
     while True:
-       
-        message = input("Digite a sua mensagem (Digite 'sair' para sair): ")  # Enviar uma mensagem
+        # Enviar uma mensagem
+        message = input("Digite a sua mensagem (Digite 'sair' para sair): ")
         if message.lower() == 'sair' :
             break
-
-        elif  (message.startswith('03')) :
-             client_socket.sendall(message.encode())
-            
-        else:
-            continue
-
-        
-        
-
-
-        
-
-            
-        
-            
-        
-            
-
-       
+        client_socket.sendall(message.encode())
 
         # Receber a resposta do servidor
         data = client_socket.recv(1024)
@@ -45,5 +26,4 @@ def start_client(server_host='10.10.56.8', server_port=42647):
 
 
 if __name__ == '__main__':
-    start_client(server_host='10.10.56.8', server_port=42647)
-
+    start_client(server_host='10.228.247.158', server_port=9941)
--- socketServerTCP.py
+++ socketServerTCP.py
@@ -6,7 +6,7 @@ import random
 HOST = '0.0.0.0'
 ports = [random.randint(1024, 49151) for _ in range(4)]  # Lista de portas aleatórias entre 1024 e 49151
 PORT = random.choice(ports)  # Seleciona uma porta aleatória da lista
-
+posix_time = time.time()
 print(f"Servidor iniciado em {HOST}:{PORT}")
 
 # Conecta ao banco de dados SQLite
@@ -15,23 +15,10 @@ cursor = conn_db.cursor()
 
 # Cria a tabela 'clientes' se ela não existir
 cursor.execute('''CREATE TABLE IF NOT EXISTS clientes (id TEXT, endereco TEXT, timestamp TEXT)''')
-# Cria a tabela 'mensagens_pendentes' se ela não existir
-cursor.execute('''CREATE TABLE IF NOT EXISTS mensagens_pendentes (src TEXT, dst TEXT, timestamp TEXT, data TEXT)''')
-
-def enviar_mensagens_pendentes(dst, conn):
-    cursor.execute("SELECT src, timestamp, data FROM mensagens_pendentes WHERE dst=?", (dst,))
-    mensagens = cursor.fetchall()
-    for msg in mensagens:
-        src, timestamp, data = msg
-        mensagem_formatada = f"Mensagem de: {src}, Horário: {timestamp}, Conteúdo: {data}"
-        conn.sendall(mensagem_formatada.encode())
-    cursor.execute("DELETE FROM mensagens_pendentes WHERE dst=?", (dst,))
-    conn_db.commit()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    clientes_conectados = {}  # Dicionário para manter o controle dos clientes conectados
     while True:
         try:
             conn, addr = s.accept()
@@ -40,7 +27,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
                 # Recebe dados do cliente
                 data = conn.recv(1024)
-                
                 if not data:
                     continue
 
@@ -51,8 +37,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 if existing_id:
                     unique_id = existing_id[0]
                     print(f"Conectado por {addr} com ID {unique_id}")
-                    clientes_conectados[unique_id] = conn  # Adiciona o cliente ao dicionário de clientes conectados
-                    enviar_mensagens_pendentes(unique_id, conn)  # Envia mensagens pendentes, se houver
                 else:
                     # Se não existe ID, envia mensagem para se cadastrar
                     conn.sendall("Parece que você não está cadastrado em nosso servidor, envie '01' para se cadastrar.".encode())
@@ -68,40 +52,23 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     conn.sendall(f"Seu ID único é: {unique_id}".encode())
 
                     # Insere o ID único e o endereço do cliente no banco de dados
-                    posix_time = time.time()
                     cursor.execute("INSERT INTO clientes (id, endereco, timestamp) VALUES (?, ?, ?)",
                                    (unique_id, addr[0], str(posix_time)))
                     conn_db.commit()
-                    clientes_conectados[unique_id] = conn  # Adiciona o cliente ao dicionário de clientes conectados
 
-                # Dentro do loop para tratamento de mensagens do cliente
+                # Loop para tratamento de mensagens do cliente
                 while True:
+                    print(f"Recebido de {addr[0]}: {data.decode()} em hora POSIX: {posix_time}")
+                    if len(data.decode()) > 218:
+                        # Envia uma mensagem de erro ao cliente
+                        conn.sendall(f"Erro: Mensagem muito grande! (Máximo de 218 caracteres)".encode())
+                    else:
+                        # Envia uma mensagem de sucesso ao cliente
+                        conn.sendall(f"Sucesso: Mensagem recebida com sucesso! em hora POSIX: {posix_time}".encode())
+
                     data = conn.recv(1024)
                     if not data:
-                        del clientes_conectados[unique_id]  # Remove o cliente do dicionário de clientes conectados
                         break  # Trata desconexão do cliente
-
-                    # Extrai destinatário_id e mensagem da string recebida
-                    try:
-                        destinatario_id, mensagem = data.decode().split(':', 1)
-                    except ValueError:
-                        # Formato inválido, não processa a mensagem
-                        print("Formato de mensagem inválido.")
-                        continue
-
-                    # Verifica se o destinatário está conectado
-                    if destinatario_id in clientes_conectados:
-                        # Envia a mensagem ao destinatário
-                        try:
-                            clientes_conectados[destinatario_id].sendall(mensagem.encode())
-                        except Exception as e:
-                            print(f"Erro ao enviar mensagem para {destinatario_id}: {e}")
-                    else:
-                        # Armazena a mensagem na base de dados para destinatários desconectados
-                        posix_time = time.time()
-                        cursor.execute("INSERT INTO mensagens_pendentes (src, dst, timestamp, data) VALUES (?, ?, ?, ?)",
-                                    (unique_id, destinatario_id, str(posix_time), mensagem))
-                        conn_db.commit()
 
         except ConnectionResetError:
             print(f"Conexão perdida com {addr}.")
